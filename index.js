9c7a2f556ba4def1ec90488df5f0347693a00aea
Or32 <96663204+Or32@users.noreply.github.com>
2023-01-23 16:14:24 +0200

    added new link feature and new line space

--- index.js
+++ index.js
@@ -20,19 +20,21 @@ app.get('/', (req, res) => {
 app.post('/answer', async (req, res) => {
   const {question, age} = req.body;
 
-
-  
-
   console.log(process.env.OPENAI_API_KEY)
   const openai = new OpenAIApi(configuration);
   const response = await openai.createCompletion({
   model: "text-davinci-003",
   prompt: `explain ${question} , to a ${age} year old`,
   max_tokens: 400,
-  temperature: 1,
+  temperature: 0.7,
+  top_p:0.9,
+
 });
+   let x = response.data.choices[0].text.replace(/(\r\n|\n|\r)/gm, "\n");
+   console.log(x)
+
   
- 
+  
   res.send(response.data.choices[0].text);
 });
 
@@ -40,4 +42,4 @@ app.listen(3000, () => {
   console.log('API endpoint listening on port 3000');
 });
 
-module.exports = app
+// module.exports = app
